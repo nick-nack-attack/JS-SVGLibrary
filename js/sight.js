142e532ad1ec80b9160c1c04c8eefbb75d2d3ce3
Nick Nack Attack <nickfarni001@gmail.com>
2020-12-22 14:59:49 -0700

    Complete module

--- js/sight.js
+++ js/sight.js
@@ -4,8 +4,34 @@ class SVGElement {
         this.namespace = 'http://www.w3.org/2000/svg';
         this.node = document.createElementNS(this.namespace, this.type);
         return this;
-    }
+    };
     attr(attrs) {
+        for (const [key, value] of Object.entries(attrs)) {
+            this.node.setAttributeNS(null, key, value)
+        }
         return this;
+    };
+    append(element) {
+        const parent = (typeof element === "string")
+            ? document.querySelector(element)
+            : element.node;
+        // this.node = parent;
+        // append child 'this node' to parent
+        parent.appendChild(this.node);
+        return this;
+    };
+}
+
+// add a class Sight
+class Sight {
+    constructor(selector, width, height) {
+        this.svg = new SVGElement('svg')
+            .attr({ viewbox: `0 0 ${width} ${height}`})
+            .append(selector);
+    };
+    draw(type, attrs) {
+        return new SVGElement(type)
+            .attr(attrs)
+            .append(this.svg);
     };
 }
